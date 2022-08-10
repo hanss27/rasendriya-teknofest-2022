b43560e7c37f1d80cbda0eed3f71c6e4aae832cb
Raihan <raihan.muhammad91@ui.ac.id>
2022-08-10 09:33:54 +0700

    update waypoint for mavlink

--- src/testing/test_waypoint.cpp
+++ src/testing/test_waypoint.cpp
@@ -263,8 +263,8 @@ int main(int argc, char **argv) {
 			// change WP NAV directly before dropping
 			ROS_INFO("Updating waypoints");
 			for(int i = 0; i <= 1; i++) {
-				waypoint_push.request.waypoints[wp_drop[i]].x_lat = int(tgt_latx * 1e7); // 1e7 is mavlink coordinate integer format
-				waypoint_push.request.waypoints[wp_drop[i]].y_long = int(tgt_lony * 1e7);
+				waypoint_push.request.waypoints[wp_drop[i]].x_lat = float(tgt_latx); // 1e7 is mavlink coordinate integer format
+				waypoint_push.request.waypoints[wp_drop[i]].y_long = float(tgt_lony);
 				ROS_INFO("WP: %d | Latitude: %f | Longitude: %f", wp_drop[i], tgt_latx, tgt_lony);
 			}
 
